10b40fd9b40009d252b2a660710ba5cb53530ac4
arkan481 <arkanharyo@gmail.com>
2023-04-28 05:43:14 +0800

    feat: pagination

---
+++ components/Pagination.vue
@@ -0,0 +1,36 @@
+<template>
+  <vue-awesome-paginate
+    pagination-container-class="pagination-container"
+    paginate-buttons-class="pagination-btn"
+    active-page-class="pagination-btn-active"
+    next-button-class="pagination-next-btn"
+    back-button-class="pagination-back-btn"
+  >
+    <template #prev-button>
+      <img src="~/assets/img/icon-left-arrow.svg" alt="Left arrow icon" />
+    </template>
+
+    <template #next-button>
+      <img src="~/assets/img/icon-right-arrow.svg" alt="Right arrow icon" />
+    </template>
+  </vue-awesome-paginate>
+</template>
+
+<style>
+.pagination-container {
+  @apply flex-wrap;
+}
+
+.pagination-btn {
+  @apply rounded-lg w-11 h-11 text-[#9CA3AF] hover:text-primary transition-colors;
+}
+
+.pagination-btn-active {
+  @apply bg-primary text-white hover:text-white;
+}
+
+.pagination-next-btn,
+.pagination-back-btn {
+  @apply flex items-center justify-center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,31 @@
+<script setup lang="ts">
+const currentPage = ref(1);
+</script>
 <template>
   <div class="container mx-auto my-5 px-5 md:px-0">
     <Breadcrumb />
-    <div>
+    <div
+      class="flex flex-col justify-center pb-3 md:p-5 max-w-7xl mx-auto items-center"
+    >
       <h1
         class="text-center mt-12 text-lg md:text-2xl md:font-medium max-w-md mx-auto"
       >
         Tambah Pengetahuan Dengan Membaca Artikel Menarik di Foodiewell
       </h1>
       <div
-        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8 justify-items-center pb-3 md:p-5 max-w-7xl mx-auto"
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8 justify-items-center mb-10"
       >
         <template v-for="i in 12" :key="i">
           <ArticleCard />
         </template>
       </div>
+      <Pagination
+        class="self-end"
+        v-model="currentPage"
+        :total-items="50"
+        :items-per-page="5"
+        :max-pages-shown="4"
+      />
     </div>
   </div>
 </template>
