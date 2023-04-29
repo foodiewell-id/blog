6716e56105a6123ef803bf462763a1192c44c9dd
arkan481 <arkanharyo@gmail.com>
2023-04-29 14:30:17 +0800

    fix: article card view

--- components/ArticleCard.vue
+++ components/ArticleCard.vue
@@ -11,18 +11,20 @@ defineProps({
 
 <template>
   <NuxtLink
-    to="#"
-    class="border border-grey-light rounded-md shadow-sm max-w-[18rem] cursor-pointer hover:border-primary focus:outline-primary transition-all"
+    :to="`/article/${article.slug}`"
+    class="border border-grey-light rounded-md shadow-sm max-w-[18rem] cursor-pointer hover:border-primary focus:outline-primary transition-all overflow-hidden"
   >
-    <article>
+    <article class="h-full">
       <img
-        class="w-full border-grey-light object-cover border-b p-2"
+        class="w-full border-grey-light object-cover border-b h-[12rem]"
         :src="article.thumbnail.url"
         :alt="article.title"
       />
-      <div class="p-5 mt-1">
+      <div class="p-5 pt-4 h-full">
         <Tag>{{ article.tag }}</Tag>
-        <h2 class="text-lg font-medium mt-2 text-black">
+        <h2
+          class="text-lg font-medium mt-2 text-black text-ellipsis line-clamp-3"
+        >
           {{ article.title }}
         </h2>
         <p class="text-grey text-sm mt-2 font-light text-ellipsis line-clamp-3">
